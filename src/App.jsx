fe6d3bb7785be7a3f1eccbbf5e40a411d33ec459
Dulain Perera <dulainp337@gmail.com>
2025-03-18 17:55:51 +0530

    create tech component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import Intro from './components/Intro';
 import Navbar from './components/navbar';
-
+import Tech from './components/Tech';
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-white antialized selectionbg-cyan-300 selection:text-cyan-900">
-      <div className="flixed top-0 -z-10 h-full w-full"></div>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    <div className="overflow-x-hidden text-white antialiased selection:bg-cyan-300 min-h-screen relative">
+      <div className="fixed top-0 -z-10 h-full w-full"></div>
+      <div className="absolute top-0 z-[-2] h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
-      <div className="container mx-auto px-8">
+      <div className="container mx-auto px-8 relative z-10 min-h-screen">
         <Navbar />
         <Intro />
+        <Tech />
       </div>
-      
     </div>
   )
 }
---
+++ src/components/Tech.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+const Tech = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <h2 className="text-4xl font-thin mt-10 mb-10 text-center">Technologies</h2>
+      <div className="flex flex-col md:flex-row gap-6 justify-between">
+        
+        <div className="flex-1 border border-gray-700 rounded-xl p-6 shadow-xl">
+          <h3 className="text-xl mb-4 font-medium">Languages</h3>
+          <ul className="space-y-2">
+            <li className="flex items-center">
+              <span className="mr-2">•</span> Python
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> Java
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> HTML
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> CSS
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> JavaScript
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> PHP
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> SQL
+            </li>
+          </ul>
+        </div>
+
+        
+        <div className="flex-1 border border-gray-700 rounded-xl p-6 shadow-xl">
+          <h3 className="text-xl mb-4 font-medium">Libraries & Frameworks</h3>
+          <ul className="space-y-2">
+            <li className="flex items-center">
+              <span className="mr-2">•</span> React
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> Tailwind
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> Bootstrap
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> Laravel
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> Spring Boot
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> Node.js
+            </li>
+          </ul>
+        </div>
+
+        
+        <div className="flex-1 border border-gray-700 rounded-xl p-6 shadow-xl">
+          <h3 className="text-xl mb-4 font-medium">Tools</h3>
+          <ul className="space-y-2">
+            <li className="flex items-center">
+              <span className="mr-2">•</span> Git/GitHub
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> VS Code
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> IntelliJ
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> Postman
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> MySQL
+            </li>
+            <li className="flex items-center">
+              <span className="mr-2">•</span> MongoDB
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Tech

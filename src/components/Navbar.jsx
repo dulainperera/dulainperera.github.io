639d0477ba1b1f9b8376a790e939017f77b0f816
Dulain Perera <dulainp337@gmail.com>
2025-03-18 20:22:31 +0530

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
 import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
-  
+
 const Navbar = () => {
-  return <nav className="mb-20 flex item-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
         <p className="text-2xl">Dulain Perera</p>
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin/>
-        <FaGithub/>
-        <FaInstagram/>
-    </div>
-  </nav>
-    
+      </div>
+      <div className="m-8 flex items-center justify-center gap-6 text-4xl">
+        <a href="https://www.linkedin.com/in/dulain-perera-091778292/" target="_blank" className="text-gray-500 hover:text-blue-700 transition duration-300 ease-in-out">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/dulainperera" target="_blank" className="text-gray-500 hover:text-black transition duration-300 ease-in-out">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/dulain_03/" target="_blank" className="text-gray-500 hover:text-pink-500 transition duration-300 ease-in-out">
+          <FaInstagram />
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;

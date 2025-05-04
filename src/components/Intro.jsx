249a98317ac20ef483977ff63ffecc893190895c
Dulain Perera <dulainp337@gmail.com>
2025-05-04 08:39:17 +0530

    change button responsiveness

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -51,7 +51,7 @@ const Intro = () => {
               <a
                 href="/Dulain_Perera_CV.pdf"
                 download
-                className="px-6 py-2 text-lg font-medium bg-gray-600 rounded-full hover:bg-gray-500 transition duration-300 ease-in-out"
+                className="px-6 py-2 text-sm md:text-lg font-medium bg-gray-600 rounded-full hover:bg-gray-500 transition duration-300 ease-in-out"
               >
                 Download CV
               </a>
@@ -60,7 +60,7 @@ const Intro = () => {
               <a
                 href="mailto:dulainp337@gmail.com"
                 target="_blank"
-                className="px-6 py-2 text-lg font-medium bg-neutral-950 text-gray-500  border border-gray-500 rounded-full hover:bg-gray-500 transition duration-300 ease-in-out hover:text-neutral-950"
+                className="px-6 py-2 text-sm md:text-lg font-medium bg-neutral-950 text-gray-500 border border-gray-500 rounded-full hover:bg-gray-500 transition duration-300 ease-in-out hover:text-neutral-950"
               >
                 Get in Touch
               </a>

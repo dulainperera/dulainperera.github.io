0f1835d7f4a39282441f04c3f22dda605de843be
Dulain Perera <dulainp337@gmail.com>
2025-06-25 16:45:14 +0530

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,61 +4,56 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="static w-300px z-50 flex items-center justify-center py-6 px-12 bg-neutral-950">
-      <div className="flex flex-shrink-0 items-center">
-        <p className="text-2xl text-gray-300">Dulain Perera</p>
-      </div>
-      <div className="hidden md:flex m-8 items-center justify-center gap-8 text-xl">
-  
-        <a href="#tech" className=" text-gray-500 hover:text-white relative transition duration-300 ease-in-out 
-        after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white 
-        after:transition-all after:duration-300 hover:after:w-full">
-          Tech
-        </a>
-        <a href="#experience" className="text-gray-500 hover:text-white relative transition duration-300 ease-in-out 
-        after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white 
-        after:transition-all after:duration-300 hover:after:w-full">
-          Experience
-        </a>
-        <a href="#projects" className="text-gray-500 hover:text-white relative transition duration-300 ease-in-out 
-        after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white 
-        after:transition-all after:duration-300 hover:after:w-full">
-          Projects
-        </a>
+    <nav className="w-full z-50 bg-neutral-950 px-6 py-4">
+      {/* Desktop Navbar */}
+      <div className="hidden md:flex items-center justify-center">
+        <div className="flex flex-shrink-0 items-center">
+          <p className="text-2xl text-gray-300">DP</p>
+        </div>
+        <div className="flex m-8 items-center justify-center gap-8 text-xl">
+          <a href="#tech" className="ml-64 text-gray-500 hover:text-gray-300 relative transition duration-300 ease-in-out 
+            ">
+            Tech
+          </a>
+          <a href="#experience" className="text-gray-500 hover:text-gray-300 relative transition duration-300 ease-in-out 
+            ">
+            Experience
+          </a>
+          <a href="#projects" className="text-gray-500 hover:text-gray-300 relative transition duration-300 ease-in-out 
+            ">
+            Projects
+          </a>
+          <a href="#contact" className="px-6 py-2 bg-neutral-950 text-gray-500 border border-gray-500 rounded-full  transition duration-300 ease-in-out hover:text-gray-300 hover:border-gray-300">
+            Get in Touch
+          </a>
+        </div>
       </div>
 
-      <div className="md:hidden flex items-center">
-        <button onClick={toggleMenu} className="text-gray-500 hover:text-white relative transition duration-300 ease-in-out">
+      {/* Mobile Navbar Header */}
+      <div className="flex md:hidden items-center justify-between">
+        <p className="text-2xl text-gray-300">DP</p>
+        <button onClick={toggleMenu} className="text-gray-500">
           {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
         </button>
       </div>
 
+      {/* Mobile Dropdown Menu */}
       {isOpen && (
-        <div className="absolute top-16 left-0 w-full bg-neutral-950 flex flex-col items-center gap-8 text-xl py-4 md:hidden">
-          <a href="#intro" className="text-gray-300 hover:text-white relative transition duration-300 ease-in-out 
-          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white 
-          after:transition-all after:duration-300 hover:after:w-full" onClick={toggleMenu}>
-            About
-          </a>
-          <a href="#tech" className="text-gray-300 hover:text-white relative transition duration-300 ease-in-out 
-          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white 
-          after:transition-all after:duration-300 hover:after:w-full" onClick={toggleMenu}>
+        <div onClick={toggleMenu} className="d:hidden fixed inset-0 top-[60px] z-40 bg-black/40 backdrop-blur-md flex flex-col items-center justify-center gap-8 text-xl text-gray-300">
+          <a href="#tech" onClick={toggleMenu} >
             Tech
           </a>
-          <a href="#experience" className="text-gray-300 hover:text-white relative transition duration-300 ease-in-out 
-          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white 
-          after:transition-all after:duration-300 hover:after:w-full" onClick={toggleMenu}>
+          <a href="#experience" onClick={toggleMenu} >
             Experience
           </a>
-          <a href="#projects" className="text-gray-300 hover:text-white relative transition duration-300 ease-in-out 
-          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white 
-          after:transition-all after:duration-300 hover:after:w-full" onClick={toggleMenu}>
+          <a href="#projects" onClick={toggleMenu} className="text-gray-300 ">
             Projects
+          </a>
+          <a href="#contact" onClick={toggleMenu} className="px-6 py-2 bg-neutral-950 text-gray-300 border border-gray-500 rounded-full hover:bg-gray-500 transition duration-300 ease-in-out hover:text-neutral-950">
+            Get in Touch
           </a>
         </div>
       )}

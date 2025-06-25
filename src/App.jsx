310592e3090ac5ecf69a69cb8bf955f71fdc2c15
Dulain Perera <dulainp337@gmail.com>
2025-06-25 20:13:05 +0530

    navbar effects changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import Contact from './components/Contact';
 import Experience from './components/Experience';
 import Intro from './components/Intro';
 import Navbar from './components/Navbar';
-//import Projects from './components/Projects';
+// import Projects from './components/Projects';
 import Tech from './components/Tech';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-white antialiased relative">
-      <div className="fixed top-0 -z-10 h-full w-full"></div>
-      <div className="absolute top-0 z-[-2] h-full w-full bg-neutral-950"></div>
+    <div className="overflow-x-hidden text-white antialiased relative min-h-screen">
+      {/* Custom background */}
+      <div className="absolute top-0 left-0 z-[-2] h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
       <Navbar />
-      <div className="container mx-auto px-8 relative z-10 min-h-screen pt-32"> 
+      <div className="container mx-auto px-8 relative z-10 min-h-screen pt-16"> 
         <div id="intro" className="scroll-mt-36"> 
           <Intro />
         </div>
@@ -23,7 +23,7 @@ const App = () => {
         <div id="experience" className="scroll-mt-36"> 
           <Experience />
         </div>
-        {/*div id="projects" className="scroll-mt-36"> 
+        {/* <div id="projects" className="scroll-mt-36"> 
           <Projects />
         </div> */}
         <div id="contact" className="scroll-mt-36"> 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="w-full z-50 bg-neutral-950 px-6 py-4">
+    <nav className="w-full z-50 px-6 py-4">
       {/* Desktop Navbar */}
       <div className="hidden md:flex items-center justify-center">
         <div className="flex flex-shrink-0 items-center">
@@ -34,15 +34,18 @@ const Navbar = () => {
 
       {/* Mobile Navbar Header */}
       <div className="flex md:hidden items-center justify-between">
+
         <p className="text-2xl text-gray-300">DP</p>
+
         <button onClick={toggleMenu} className="text-gray-500">
           {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
         </button>
+
       </div>
 
       {/* Mobile Dropdown Menu */}
       {isOpen && (
-        <div onClick={toggleMenu} className="d:hidden fixed inset-0 top-[60px] z-40 bg-black/40 backdrop-blur-md flex flex-col items-center justify-center gap-8 text-xl text-gray-300">
+        <div onClick={toggleMenu} className="md:hidden fixed inset-0 top-[60px] z-40 bg-black/60 backdrop-blur-md flex flex-col items-center justify-center gap-8 text-xl text-gray-300">
           <a href="#tech" onClick={toggleMenu} >
             Tech
           </a>
@@ -52,7 +55,7 @@ const Navbar = () => {
           <a href="#projects" onClick={toggleMenu} className="text-gray-300 ">
             Projects
           </a>
-          <a href="#contact" onClick={toggleMenu} className="px-6 py-2 bg-neutral-950 text-gray-300 border border-gray-500 rounded-full hover:bg-gray-500 transition duration-300 ease-in-out hover:text-neutral-950">
+           <a href="#contact" className="px-6 py-2 bg-neutral-950 text-gray-500 border border-gray-500 rounded-full  transition duration-300 ease-in-out hover:text-gray-300 hover:border-gray-300">
             Get in Touch
           </a>
         </div>
